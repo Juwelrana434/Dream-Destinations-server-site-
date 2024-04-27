51a1a90e1fb62a4db378536a393544bb561038c2
Juwelrana434 <jewel.rana434@gmail.com>
2024-04-27 13:19:18 +0600

    tourist spots section data servser uoload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const tourspots = require('./Tourists spot.json');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,9 +8,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
+// Tourists spot section data upload to mongodb server
+app.get('/tourspots', (req, res) => {
+res.send(tourspots);
+})
+app.get('/tourspots/:id', (req, res) =>{
+const id = parseInt(req.params.id);
+console.log(id);
+const tourspot = tourspots.find(tourspot => tourspot.id === id) || {};
+res.send(tourspot);
+})
 
 // for server run  
 app.get('/', (req, res) =>{
