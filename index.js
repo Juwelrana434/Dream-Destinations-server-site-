10ac3f573131e54057febcaed8ebb770a3d55d20
Juwelrana434 <jewel.rana434@gmail.com>
2024-04-27 17:38:21 +0600

    user pass set

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const tourspots = require('./Tourists spot.json');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,7 +10,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Tourists spot section data upload to mongodb server
+
+const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASS}@cluster0.ev00748.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+// Tourists spot section data upload to mongodb server start
 app.get('/tourspots', (req, res) => {
 res.send(tourspots);
 })
@@ -18,6 +48,7 @@ console.log(id);
 const tourspot = tourspots.find(tourspot => tourspot.id === id) || {};
 res.send(tourspot);
 })
+// Tourists spot section data upload to mongodb server end
 
 // for server run  
 app.get('/', (req, res) =>{
